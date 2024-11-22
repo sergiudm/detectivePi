ea6ab45530ecca05123dfd421e3d2732bc8ca2e3
Surgiu <113337694+Surgiu@users.noreply.github.com>
2024-11-22 12:52:57 +0800

    Update server IP address in client.py and modify boolean array representation; enhance shutdown handling in socket connection. Update email credentials in config.json.

--- communication/client.py
+++ communication/client.py
@@ -2,7 +2,7 @@ import socket
 import struct
 
 # 设置服务器的IP地址和端口号
-server_ip = '192.168.1.X'  # 替换X为服务器的实际IP地址
+server_ip = "10.13.220.234"  # 替换X为服务器的实际IP地址
 server_port = 12345
 
 # 创建一个socket对象
@@ -13,10 +13,10 @@ client_socket.connect((server_ip, server_port))
 
 try:
     # 创建一个8位的布尔数组
-    bool_array = [True, False, True, True, False, False, True, False]  # 示例数组
+    bool_array = [1, 0, 0, 1, 0, 0, 1, 0]  # 示例数组
 
     # 将布尔数组打包成一个字节
-    packed_data = struct.pack('B', int(''.join(map(str, bool_array)), 2))
+    packed_data = struct.pack("B", int("".join(map(str, bool_array)), 2))
 
     # 发送数据到服务器
     client_socket.sendall(packed_data)
@@ -24,6 +24,11 @@ try:
     # 接收服务器的响应
     response = client_socket.recv(1024)
     print(f"Received from server: {response.decode('utf-8')}")
+except KeyboardInterrupt:
+    # shutdown the connection
+    client_socket.shutdown(socket.SHUT_RDWR)
+    client_socket.close()
+    print("Exit")
 finally:
     # 关闭连接
     client_socket.close()
