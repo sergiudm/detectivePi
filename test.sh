892565f943727054a925ae587eeb1d2a738158f3
qijun han <surgiu_c@outlook.com>
2024-12-19 17:36:07 +0800

    Update test.sh: change execution command to use module syntax for runner_engine

--- test.sh
+++ test.sh
@@ -2,5 +2,5 @@
 
 PYTHON_INTERPRETER=$(which python3)
 echo "Current Python interpreter: $PYTHON_INTERPRETER"
-cd detective/runner
-sudo $PYTHON_INTERPRETER utils.py
+
+sudo $PYTHON_INTERPRETER -m detective.runner.runner_engine
