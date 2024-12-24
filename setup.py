bc62f02c34a41d276d42320c541a31194187d5cf
qijun han <surgiu_c@outlook.com>
2024-12-24 22:55:33 +0800

    chore: update package metadata and change project name to Thread-Everything

--- setup.py
+++ setup.py
@@ -1,27 +1,21 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="detective_pi",
-    version="0.2.0",  
-    author="Sergiu Han",  
-    author_email="sergiudm@outlook.com",  
-    description="A versatile tool for daily use",
+    name="Thread-Everything",
+    version="0.3.0",
+    author="Sergiu Han, Jihan Li",
+    author_email="sergiudm@outlook.com, 12211820@mail.sustech.edu.cn",
+    description="An easy-to-use interface to run threads from different machines",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
-    url="https://github.com/sergiudm/detective", 
+    url="https://sergiudm.github.io/Thread-Everything/",
     packages=find_packages(include=["detective", "detective.*"]),
-    install_requires=[
-        "opencv-python",
-        "opencv-python-headless",
-        "mediapipe",
-        "pygame"
-    ],
+    install_requires=["opencv-python", "opencv-python-headless", "mediapipe", "pygame"],
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License", 
+        "License :: OSI Approved :: Apache Software License",
         "Operating System :: OS Independent",
     ],
-    python_requires=">=3.10",  
-    entry_points={  
-    },
+    python_requires=">=3.10",
+    entry_points={},
 )
